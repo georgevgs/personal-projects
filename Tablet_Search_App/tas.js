8a1186b583966413209faab034096a289553535c
georgevgs <georgevagdas@protonmail.com>
2019-07-05 16:04:42 +0300

    TSA

--- Tablet_Search_App/tas.js
+++ Tablet_Search_App/tas.js
@@ -2,3 +2,22 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+<script type="text/javascript" src="test.js"></script>
+
+$.ajax({
+        type: "GET",
+        url: "http://northwind.servicestack.net/customers.json",
+        cache: false,
+        data: dataString,
+
+        success: function (html) {
+            //$("#more").after(html);
+            alert("Success!");
+        }
+    });
+
+$.getJSON('http://northwind.servicestack.net/customers.json' + dataString, function (json_data) {
+    alert(JSON.stringify(json_data));
+});    
