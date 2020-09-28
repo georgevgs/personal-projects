ca02b4f975cfcf4b32d31e1ecaf6121e532e1a24
George <vagdas@pm.me>
2020-09-28 23:17:02 +0300

    Footer

--- Angular/thekitchenstudio/src/app/app.module.ts
+++ Angular/thekitchenstudio/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarouselComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
