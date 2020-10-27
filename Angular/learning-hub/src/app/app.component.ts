4c0aeee9919aca002d84294e6cd1772745164eb3
George <vagdas@pm.me>
2020-10-27 18:14:44 +0200

    Input - Output

--- Angular/learning-hub/src/app/app.component.ts
+++ Angular/learning-hub/src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app';
+  rootName = 'George';
+
+  rootItems = ['Apples', 'Bananas', 'Cherries'];
+
+  onItemWasAdded(newItem): void {
+    this.rootItems.push(newItem);
+    console.log(this.rootItems);
+  }
+
+  onNameChanged(newName): void {
+    this.rootName = newName;
+  }
 }
--- Angular/learning-hub/src/app/app.module.ts
+++ Angular/learning-hub/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user.component';
+import { FormsModule } from '@angular/forms';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    UserDetailComponent,
+    CartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
